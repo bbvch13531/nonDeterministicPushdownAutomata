f8a0ed230ce52ac4d3e582f5439ff56a050d731a
bbvch13531 <bbvch13531@gmail.com>
2019-02-17 11:00:39 +0900

    Initial commit
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ DeterministicPushdownAutomata/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DeterministicPushdownAutomata
+//
+//  Created by KyungYoung Heo on 17/02/2019.
+//  Copyright © 2019 KyungYoung Heo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
